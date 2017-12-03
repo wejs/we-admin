9968d95ffb6b2b675f61195bc7e66c691485cfa2
albertosouza <alberto.souza.99@gmail.com>
2017-12-03 15:21:33 -0200

    add support for upload images to we.js host with we.js plugin

--- addon/controllers/application.js
+++ addon/controllers/application.js
@@ -2,7 +2,35 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   settings: Ember.inject.service('settings'),
+  upload: Ember.inject.service('upload'),
+
   settingsLoaded: Ember.computed.alias('settings.loaded'),
   appName: Ember.computed.alias('settings.data.appName'),
-  appLogo: Ember.computed.alias('settings.data.appLogo')
+  appLogo: Ember.computed.alias('settings.data.appLogo'),
+  fileSelectorModalOpen: Ember.computed.alias('upload.modalOpen'),
+
+  actions: {
+    // UPLOAD ACTIONS:
+    upload() {
+      this.get('upload').upload();
+    },
+
+    selected() {
+      this.get('upload').selected(...arguments);
+    },
+
+    onHideUploadModal() {
+      this.get('upload').onHideUploadModal();
+    },
+
+    progress(uploader, e) {
+      this.get('upload').progress(uploader, e);
+    },
+    didUpload(uploader, e) {
+      this.get('upload').didUpload(uploader, e);
+    },
+    didError(uploader, jqXHR, textStatus, errorThrown) {
+      this.get('upload').didError(uploader, jqXHR, textStatus, errorThrown);
+    },
+  }
 });
