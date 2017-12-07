6834a48c6fe752fa99097fcd312ad2b0b0a20666
albertosouza <alberto.souza.99@gmail.com>
2017-12-07 17:38:28 -0200

    fix date-time-picker component

--- addon/components/date-time-picker.js
+++ addon/components/date-time-picker.js
@@ -2,23 +2,31 @@ import Ember from 'ember';
 import EmberFlatpickr from 'ember-flatpickr/components/ember-flatpickr';
 
 export default EmberFlatpickr.extend({
-  session: Ember.inject.service('session'),
-  altFormat: 'j/n/Y H:i',
-  enableSeconds: false,
-  enableTime: true,
-  allowInput: false,
-  altInput: true,
-  clickOpens: true,
-  altInputClass: 'form-control',
-  locale: 'pt',
-  mode: 'single',
-  nextArrow: '>',
-  parseDate: false,
-  placeholder: '',
-  prevArrow: '<',
-  static: false,
-  dateFormat: 'Z',
-  timeFormat: 'H:i',
-  utc: false,
-  wrap: false
+  init: function() {
+    const defaults = {
+      altFormat: 'j/n/Y H:i',
+      enableSeconds: false,
+      enableTime: true,
+      allowInput: false,
+      altInput: true,
+      clickOpens: true,
+      altInputClass: 'form-control',
+      locale: 'pt',
+      mode: 'single',
+      nextArrow: '>',
+      parseDate: false,
+      placeholder: '',
+      prevArrow: '<',
+      static: false,
+      // dateFormat: 'Z',
+      // timeFormat: 'H:i',
+      utc: false,
+      wrap: false
+    };
+
+    Ember.setProperties(this.attrs, defaults);
+    Ember.setProperties(this, defaults);
+
+    this._super(...arguments);
+  }
 });
