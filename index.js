65f098bdf13fde8611c795538d70d14e4316be8c
albertosouza <alberto.souza.99@gmail.com>
2018-01-08 16:54:26 -0200

    add support for move prod build files to prod folder

--- index.js
+++ index.js
@@ -7,5 +7,22 @@ module.exports = {
     this._super.included(app);
     app.import('vendor/menu/menu-links-sortable.css');
     app.import('vendor/Sortable.js');
+  },
+
+  outputReady: function () {
+    // only run in production env:
+    if (this.app.env !== 'production') {
+      return null;
+    }
+
+    console.log('this', this);
+
+    var fs = this.project.require('fs-extra');
+    var originFolder = process.cwd() + '/dist';
+    var distFolder = process.cwd() + '/prod';
+    // cleanup:
+    fs.emptyDirSync(distFolder);
+    // copy:
+    fs.copySync(originFolder, distFolder);
   }
 };
