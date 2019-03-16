f0e9f679282761cf9528fb9ecb3cadef3da4a32b
albertosouza <alberto.souza.99@gmail.com>
2019-03-16 15:39:51 -0300

    add mt-published component to show publication status on lists

---
+++ app/components/mt-published.js
@@ -0,0 +1,31 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import moment from 'moment';
+
+export default Component.extend({
+  publishStatus: computed('record.publishedAt', 'record.published', function() {
+    let publishedAt = this.get('record.publishedAt');
+    let published = this.get('record.published');
+
+    if (published) {
+      return 'published';
+    }
+
+    if (publishedAt) {
+      let unixNow = moment().unix();
+
+      let p = moment(publishedAt);
+      if (p.isValid()) {
+        let pUnix = p.unix();
+
+        if (pUnix >= unixNow) {
+          return 'scheduled-future';
+        } else {
+          return 'scheduled-old';
+        }
+      }
+    }
+
+    return 'unpublished';
+  })
+});
